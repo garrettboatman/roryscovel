280d2e7f587526a6c3a2671352267e726c3e566d
Garrett Boatman <garrett@getrecord.com>
2023-03-31 10:45:37 -0700

    cookieless GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,20 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-0MPV3WHJ3W", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          client_storage: 'none',
+        },
+      },
+    },
     'gatsby-plugin-postcss',
     `gatsby-plugin-image`,
     {
