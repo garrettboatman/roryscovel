054545546a659e213a521fd99858ebae4b47a468
Garrett Boatman <hello@garrettboatman.com>
2024-11-23 12:02:24 -0800

    color header

--- src/pages/index.js
+++ src/pages/index.js
@@ -205,7 +205,7 @@ const IndexPage = ({ data }) => {
       </div>
       <br />
       <br />
-      <div className="w-full relative z-10 md:min-w-[380px] md:max-w-[480px] md:px-4 mx-auto mb-4">
+      <div id="tickets" className="w-full relative z-10 md:min-w-[380px] md:max-w-[480px] md:px-4 mx-auto mb-4">
         <div className="relative mt-10 mb-[-30px]">
           <div className="w-full text-center">
             <span className="w-[156px] inline-block bold bg-[#ebf3f6] border-2 border-[#03a8e6] text-[#03a8e6] px-[15px] py-[3px] rounded-[100px] text-sm font-bold">
